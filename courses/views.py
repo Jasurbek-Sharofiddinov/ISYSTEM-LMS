17c6b00e7cb9e7c7eb70af75bae95ea59fd96f1f
Jasurbek-Sharofiddinov <jasurbeksharofiddinov62@gmail.com>
2021-07-28 03:58:18 +0500

    i have added 3 courses table by using for loop in our index.html file

--- courses/views.py
+++ courses/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 
 # Create your views here.
 
 def index(request):
-	return HttpResponse('Salomlar')
+	template = loader.get_template('courses/index.html')
+
+	context = {
+		'courses': [
+			{
+				'id': 1,
+				'name': 'Course 1'
+			},
+			{
+				'id': 2,
+				'name': 'Course 2'
+			},
+			{
+				'id': 3,
+				'name': 'Course 3'
+			}
+
+		]
+	}
+	return HttpResponse(template.render(context, request))
+
+	return render('templates/courses/index.html')

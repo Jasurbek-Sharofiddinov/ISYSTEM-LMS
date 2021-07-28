c54354dcc37c56caa6a2799749e74ee4ee9896a8
Jasurbek-Sharofiddinov <jasurbeksharofiddinov62@gmail.com>
2021-07-28 16:50:24 +0500

    i added database for the course model i called it Lists

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Lists(models.Model):
+	id= models.IntegerField(primary_key=True)
+	courseCode= models.CharField(max_length=255)
+	name=models.CharField(max_length=255)
+	price=models.DecimalField(max_digits=10,decimal_places=2,null=True)
+	description=models.TextField(blank=True)
+	status=models.BooleanField()
+	deleted_at=models.DateTimeField(auto_now=False, auto_now_add=False)
+
+
+
